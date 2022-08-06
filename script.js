e661327b53a0682472ce075021fc124a78d73dd6
reverse <68628837+rxverse@users.noreply.github.com>
2022-08-06 15:37:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+button = document.getElementById("button");
+error = document.getElementById("error");
+button.addEventListener("click", function() {
+    error.textContent = "Error, please try again";
+});
